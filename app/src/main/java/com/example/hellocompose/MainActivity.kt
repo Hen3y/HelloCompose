2667da73a7489b17c1364077f92ce787b613dd2c
Yukun <yukun.han@thoughtworks.com>
2022-02-23 00:07:56 +0800

    add icon button and change implementation of expanding animation

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,53 +57,83 @@ fun MyApp() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun CardContent(name: String) {
     // 使用mutableStateOf()创建一个state
     // 用remember在多次recomposition间保存state的值
 
     // val expanded = remember(calculation = { -> mutableStateOf(false) })
     // line above could be simplified as below
     // val expanded = remember { mutableStateOf(false) }
-    val expanded = rememberSaveable { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
     /*
         Any animation created with animate*AsState is interruptible.
         This means that if the target value changes in the middle of the animation,
         animate*AsState restarts the animation and points to the new value.
      */
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+//    val extraPadding by animateDpAsState(
+//        targetValue = if (expanded.value) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
 
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello,")
-                // copy() allows to customize existing predefined styles
-                Text(
-                    text = "Index: $name", style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            // 允许父级modifier监听子modifier的size连续变化从而产生动画效果
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
-            }
-            OutlinedButton(
-                onClick = {
-                    expanded.value = !expanded.value
-                }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            // copy() allows to customize existing predefined styles
+            Text(
+                text = "Index: $name",
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(text = "Congratulations! You learned the basics of Compose! ".repeat(3))
             }
         }
+//        OutlinedButton(
+//            onClick = {
+//                expanded.value = !expanded.value
+//            }) {
+//            Text(text = if (expanded.value) "Show less" else "Show more")
+//        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    // change Surface to Material Design Card
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name = name)
     }
 }
 
