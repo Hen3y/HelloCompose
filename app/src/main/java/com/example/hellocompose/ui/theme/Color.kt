6f7ce678b0e16fe062b6f002930c79125a7718b7
Yukun <yukun.han@thoughtworks.com>
2022-03-02 21:10:48 +0800

    comment out old colors

--- app/src/main/java/com/example/hellocompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/hellocompose/ui/theme/Color.kt
@@ -3,10 +3,13 @@ package com.example.hellocompose.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Color值为十六进制argb
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+// old colors
+//val Purple200 = Color(0xFFBB86FC)
+//val Purple500 = Color(0xFF6200EE)
+//val Purple700 = Color(0xFF3700B3)
+//val Teal200 = Color(0xFF03DAC5)
+
 val Navy = Color(0xFF073042)
 val Blue = Color(0xFF4285F4)
 val LightBlue = Color(0xFFD7EFFE)
