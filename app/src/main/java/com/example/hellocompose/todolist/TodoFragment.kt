795bfdf664f55b1eecd3caa75f93bd8d4c350ab8
Yukun <yukun.han@thoughtworks.com>
2022-03-11 16:53:31 +0800

    extract todo row

--- app/src/main/java/com/example/hellocompose/todolist/TodoFragment.kt
+++ app/src/main/java/com/example/hellocompose/todolist/TodoFragment.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.hellocompose.ui.theme.HelloComposeTheme
@@ -45,7 +53,6 @@ class TodoFragment : Fragment() {
     // 在onCreateView之后运行
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
     }
 
     // 在onCreateView之前运行
@@ -56,7 +63,7 @@ class TodoFragment : Fragment() {
 }
 
 @Composable
-private fun TodoScreen(todoViewModel: TodoViewModel) {
+fun TodoScreen(todoViewModel: TodoViewModel) {
     // .observeAsState 会观察 LiveData<T> 并将其转换为 State<T> 对象，让 Compose 可以响应值的变化
     // listOf() 是一个初始值，用于避免在初始化 LiveData 之前可能出现 null 结果
     // by 是 Kotlin 中的属性委托语法，使我们可以自动将 State<List<TodoItem>> 从 observeAsState 解封为标准 List<TodoItem>
@@ -69,7 +76,7 @@ private fun TodoScreen(todoViewModel: TodoViewModel) {
 }
 
 @Composable
-fun TodoList(
+private fun TodoList(
     items: List<TodoItem>,
     onAddItem: (TodoItem) -> Unit,
     onRemoveItem: (TodoItem) -> Unit
@@ -80,10 +87,8 @@ fun TodoList(
                 modifier = Modifier.weight(1f),
                 contentPadding = PaddingValues(16.dp)
             ) {
-                items(items) {
-                    Row {
-                        Text(text = it.task)
-                    }
+                items(items) { item ->
+                    TodoRow(item) { onAddItem(item) }
                 }
             }
             Button(
@@ -91,9 +96,37 @@ fun TodoList(
                     .padding(16.dp)
                     .fillMaxWidth(),
                 onClick = { onAddItem(TodoItem("Test")) }) {
-                Text(text = "Add An Item")
+                Text(
+                    fontSize = MaterialTheme.typography.body1.copy(fontSize = 24.sp).fontSize,
+                    text = "Add An Item"
+                )
             }
         }
+    }
+}
+
+@Composable
+private fun TodoRow(
+    item: TodoItem,
+    modifier: Modifier = Modifier,
+    onItemClicked: (TodoItem) -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .clickable { onItemClicked(item) },
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            fontSize = MaterialTheme.typography.body1.copy(fontSize = 24.sp).fontSize,
+            text = item.task
+        )
+        Icon(
+            imageVector = item.icon.imageVector,
+            contentDescription = stringResource(id = item.icon.contentDescription)
+        )
     }
 }
 
